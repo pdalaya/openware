50136c7221564e2bf3a5e7e6fe8b5d410873b541
Aidan Waite <aidanaidan@gmail.com>
2020-10-18 23:45:14 +0900

    Awp minigame

---
+++ Assets/Scripts/DestroyAfterDelay.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterDelay : MonoBehaviour
+{
+    IEnumerator Start() {
+        yield return new WaitForSeconds(3);
+        Destroy(gameObject);
+    }
+}
