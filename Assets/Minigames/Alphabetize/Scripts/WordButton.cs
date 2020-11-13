70b73a9d71f755e551c9c52e7d54e934962de394
Aidan Waite <aidanaidan@gmail.com>
2020-11-13 09:16:01 +0900

    Alphabetize minigame

---
+++ Assets/Minigames/Alphabetize/Scripts/WordButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Alphabetize {
+    public class WordButton : MonoBehaviour {
+        AlphabetizeGameManager GameManager;
+
+        private void Awake() {
+            GameManager = GameObject.Find("AlphabetizeGameManager").GetComponent<AlphabetizeGameManager>();
+        }
+
+        public void DidTapButton() {
+            GameManager.DidTapWord(gameObject);
+        }
+    }
+}
